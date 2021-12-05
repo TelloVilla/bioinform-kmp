02abd1d3c04f78626931bbeb74cf71a20e38e8d8
TelloVilla <TelloVilla@protonmail.com>
2021-12-05 17:59:25 -0600

    commented code

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,13 @@ import _ from "lodash";
 
 // markup
 const IndexPage = () => {
+  //State values used to render elements to pages using React
   const [input, setInput] = React.useState("");
   const [compare, setCompare] = React.useState("");
   const [stepsLPS, setStepsLPS] = React.useState([]);
   const [stepsKMP, setStepsKMP] = React.useState([]);
   const [show, setShow] = React.useState(false);
+  //Initalized LPS steps array with step 0 filled
   const lpsSteps = [{
       step: 0,
       i: 1,
@@ -19,16 +21,15 @@ const IndexPage = () => {
       lps: [0],
       instr: "lps[0] is always 0"
     }];
-
+  //Initalized KMP steps array
   const kmpSteps = [];
-
+  //function to reset the steps array
   function resetSteps(){
     lpsSteps.splice(1, lpsSteps.length-1);
     kmpSteps.splice(1,kmpSteps.length-1);
   }
-
+  //transfroms LPS step data into a web component for display
   function lpsStepMaker(s){
-
     return(
       <Row key={s.step} className="step">
         <h4>Step: {s.step}</h4>
@@ -50,6 +51,7 @@ const IndexPage = () => {
     )
 
   }
+  //transfroms KMP step data into a web component for display
   function kmpStepMaker(s){
     return(
       <Row key={s.step} className="step">
@@ -86,7 +88,7 @@ const IndexPage = () => {
     let lps = lpsArray(input, compare);
     let pattern = []
     setStepsLPS(lpsSteps);
-
+    //Starting step values
     let newStep = {
       string: input,
       comp: compare,
@@ -97,7 +99,7 @@ const IndexPage = () => {
       instr: "Starting Values",
       lps: lps
     }
-
+    //clone is used to insert a new copy of the step object to prevent the reference to the same object being passed
     kmpSteps[0] = _.clone(newStep);
 
 
@@ -106,6 +108,7 @@ const IndexPage = () => {
     let j = 0;
     while(i < n){
       if(compare[j] === input[i]){
+        //input step values and instructions depending on the branch taken
         step++;
         newStep.instr = "i = " + i + " j = " + j + ", String[i] = " + input[i] + " and Pat[j] = " + compare[j] + ", match so increase i and j"
         j++;
@@ -113,9 +116,11 @@ const IndexPage = () => {
         newStep.i++;
         newStep.j++;
         newStep.step++;
+        //clone step to keep track of each step at that moment
         kmpSteps[step] = _.clone(newStep);
       }
       if(j === m){
+        //input step values and instructions depending on the branch taken
         step++;
         newStep.instr = "j = " + j + " = length of pattern, pattern found at " + (i-j) + ", set j to lps[j-1] = " + lps[j-1]
         pattern.push((i-j))
@@ -127,6 +132,7 @@ const IndexPage = () => {
 
       }else if( i < n && compare[j] !== input[i]){
         if(j !== 0){
+          //input step values and instructions depending on the branch taken
           step++;
           newStep.instr = "i = " + i + " j = " + j + ", String[i] = " + input[i] + " and Pat[j] = " + compare[j] + ", do not match and j = " + j + " which is > 0, set j to lps[j-1] = " + lps[j-1]
           j = lps[j-1];
@@ -134,6 +140,7 @@ const IndexPage = () => {
           newStep.step++;
           kmpSteps[step] = _.clone(newStep);
         }else{
+          //input step values and instructions depending on the branch taken
           step++;
           newStep.instr = "i = " + i + " j = " + j + ", String[i] = " + input[i] + " and Pat[j] = " + compare[j] + ", do not match and j = 0, so increase i"
           i++;
@@ -143,6 +150,7 @@ const IndexPage = () => {
         }
       }
     }
+    //final step to mark completion
     newStep.instr = "Finished as i >= length of string = " + n
     newStep.step++
     kmpSteps[step+1] = _.clone(newStep)
@@ -155,6 +163,7 @@ const IndexPage = () => {
     lps[0] = 0;
     let i = 1;
     let step = 0;
+    //initial step values
     let newStep = {
       step: 0,
       i: 0,
@@ -165,6 +174,7 @@ const IndexPage = () => {
     while(i < compare.length){
       step++;
       if(compare[i] === compare[length]){
+        //input step values and instructions depending on the branch taken
         newStep.instr = "i = " + i + " and length = " + length + ", Pat[length] = " + compare[length] + " and Pat[i] = " + compare[i] + ", match so increase length and i and set lps[i] = length"
         length++;
         lps[i] = length;
@@ -177,6 +187,7 @@ const IndexPage = () => {
         
       }else{
         if(length !== 0){
+          //input step values and instructions depending on the branch taken
           newStep.instr = "i = " + i + " and length = " + length + ", Pat[length] = " + compare[length] + " and Pat[i] = " + compare[i] + ", do not match and length is > 0, so length = lps[length - 1]"
           length = lps[length - 1];
           newStep.step = lpsSteps.length++;
@@ -186,6 +197,7 @@ const IndexPage = () => {
           lpsSteps[step] = _.clone(newStep)
 
         }else{
+          //input step values and instructions depending on the branch taken
           newStep.instr = "i = " + i + " and length = " + length + ", Pat[length] = " + compare[length] + " and Pat[i] = " + compare[i] + ", do not match and length is = 0, so lps[i] = 0 and increase i"
           lps[i] = 0;
           i++;
@@ -198,17 +210,22 @@ const IndexPage = () => {
 
       }
     }
+    //Final step to mark completion
     newStep.instr = "Finished as i > Pattern length = " + compare.length
     newStep.step = lpsSteps.length++;
     lpsSteps[step+1] = _.clone(newStep)
     return lps;
   }
+  //handle submit and input capture values to the KMP and LPS functions
   function handleSubmit(){
+    //Set a 30 character limit to prevent performance issues
     if(input.length > 30 || compare.length > 30){
       setShow(true);
       return;
     }
+    //Call reset function to allow new inputs to be calculated
     resetSteps();
+    //Pass the string and pattern to the kmp function
     kmpSearch(input, compare);
     setStepsKMP(kmpSteps)
   }
